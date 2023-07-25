bd1f6f16ee1cbd6bf580846109cc056199014988
Efe Polat <mefepolat@hotmail.com>
2023-07-25 18:37:07 -0400

    tailwind settings

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,20 @@
 import Image from 'next/image'
-import Head from 'next/head'
+import Head from 'next/head';
+import Home from './home';
 
 
-export default function Home() {
+export default function Index() {
   return (
    <div>
     <Head>
       <title>Create Next App</title>
       <meta name='description' content='xxxxxx' />
       <link rel="icon" href="/favicon.ico" />
+      <link rel="preconnect" href="https://fonts.googleapis.com" />
+<link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+
     </Head>
-    <h1 className='text-center h-screen bg-primary'>Food Ordering</h1>
+    <Home />
    </div>
   )
 }
