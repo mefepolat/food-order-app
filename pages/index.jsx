0978defa6c82102d8d08a3bd97752eda71d0fe90
Efe Polat <mefepolat@hotmail.com>
2023-07-25 17:25:55 -0400

    first commit food order app config

---
+++ pages/index.jsx
@@ -0,0 +1,16 @@
+import Image from 'next/image'
+import Head from 'next/head'
+
+
+export default function Home() {
+  return (
+   <div>
+    <Head>
+      <title>Create Next App</title>
+      <meta name='description' content='xxxxxx' />
+      <link rel="icon" href="/favicon.ico" />
+    </Head>
+    <h1 className='text-center h-screen bg-primary'>Food Ordering</h1>
+   </div>
+  )
+}
